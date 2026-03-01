37ceb2157a03e7daca6c363e9cc2455ad8293b25
Nayansoni2004 <nayansoni142004@gmail.com>
2026-03-02 02:21:39 +0530

    learned synchronous execution in JavaScript — code runs line by line in order, and the next statement executes only after the previous one finishes

---
+++ learn_js/16_sync.js
@@ -0,0 +1,16 @@
+/**
+ * synchronous processing
+ */
+console.log("Hello from first line");
+
+function hello() {
+    console.log("Hello from inside the function");
+}
+hello() //call
+
+console.log("Hello from last line");
+
+// OUTPUT:
+// Hello from first line
+// Hello from inside the function
+// Hello from last line
